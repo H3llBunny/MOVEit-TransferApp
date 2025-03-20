66f2ce249be58f2e3740cec437b9dcaf67c16c32
H3llBunny <bogo.dishlyanov@gmail.com>
2025-03-20 20:58:55 +0200

    Initial commit.

---
+++ MOVEit-TransferApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MOVEit_TransferApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
